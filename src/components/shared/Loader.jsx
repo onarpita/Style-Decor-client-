cae400082a6a70d7eb8ea44e65dd51bf7537781d
onarpita <onarpita14@gmail.com>
2026-02-15 23:07:07 +0600

    loader section

---
+++ src/components/shared/Loader.jsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <span className="loading loading-bars w-12"></span>
+        </div>
+    );
+};
+
+export default Loader;
